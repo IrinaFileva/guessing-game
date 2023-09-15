9e173c8e088a674d9b2102e6c069f9098eef981e
IrinaFileva <irinkaskai@gmail.com>
2023-09-15 19:47:29 +0500

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max;
+        this.min;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return this.candidate = Math.ceil((this.min + this.max) / 2)
     }
 
     lower() {
-
+        this.max = this.candidate
     }
 
     greater() {
-
+        this.min = this.candidate
     }
 }
 
